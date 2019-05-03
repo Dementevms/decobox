c109c74996a95111b4115b5674bd321e6d762f35
Dementevms <dementevms@gmail.com>
2019-05-03 20:58:42 +0500

    Подготовка проета

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/scripts.js',
+    output: {
+        filename: 'bandel.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        watchContentBase: true,
+        compress: true,
+        port: 8080
+    },
+    module:{
+        rules:[
+            {
+                test: /\$.js/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
